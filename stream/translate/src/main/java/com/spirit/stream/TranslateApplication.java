1d21fb28dc4290714379c9474878334a9012ca5c
王欢 <wanghuan@beadwallet.com>
2019-07-22 11:59:31 +0800

    api gateway

--- stream/translate/src/main/java/com/spirit/stream/TranslateApplication.java
+++ stream/translate/src/main/java/com/spirit/stream/TranslateApplication.java
@@ -2,12 +2,20 @@ package com.spirit.stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TranslateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TranslateApplication.class, args);
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "success";
     }
 
 }
