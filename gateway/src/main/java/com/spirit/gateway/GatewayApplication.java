bad1c91954e017a43575474c1d549cd627975881
王欢 <wanghuan@beadwallet.com>
2019-07-22 17:50:31 +0800

    configure

--- gateway/src/main/java/com/spirit/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/spirit/gateway/GatewayApplication.java
@@ -3,10 +3,9 @@ package com.spirit.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @EnableDiscoveryClient
-@EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
