b153415b47fd1dc772f21f47d6b009f7f4b7b2c1
王欢 <wanghuan@beadwallet.com>
2019-07-25 11:36:18 +0800

    add root pom

---
+++ translate/src/main/java/com/spirit/stream/controller/TranslateTaskController.java
@@ -0,0 +1,39 @@
+package com.spirit.stream.controller;
+
+import com.spirit.stream.dao.entity.Event;
+
+import com.spirit.stream.service.TranslateTaskService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class TranslateTaskController {
+
+    @Autowired
+    private TranslateTaskService translateTaskService;
+
+    @RequestMapping("/upload")
+    public String upload(@RequestParam("name") String name){
+        log.info("TranslateApplication: upload");
+        return name + " upload";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public void addTask(@RequestBody Event event){
+        translateTaskService.addTask(event);
+    }
+
+//    @RequestMapping(value = "/remove/{id}", method = RequestMethod.POST)
+//    public String remove(@PathVariable Long id){
+//        translateService.delete(id);
+//        return "OK";
+//    }
+//
+//    @RequestMapping(value = "/modify", method = RequestMethod.POST)
+//    public String modify(@RequestBody TranslateInfo info){
+//        translateService.update(info);
+//        return "OK";
+//    }
+}
